8d58c516f84101eca916af5c6aec338901ebdec9
Rodrigo Valla <rodrigovalla@protonmail.ch>
2023-06-05 19:08:18 -0300

    First examples...

---
+++ src/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() { //El programa siempre empieza por acá.
+    //Vamos a imprimir un mensaje:
+    std::cout << "Hola, simplemente escribimos una línea aquí.";
+    std::cout << "\n"; //Hace falta agregar el salto de línea.
+    return 0; //main() siempre devuelve un número.
+}
